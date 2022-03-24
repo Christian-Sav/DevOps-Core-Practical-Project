4f34696a06cc4d088c6d63ff5759d02654976fb5
Christian-Sav <chris.sav@outlook.com>
2022-03-24 12:03:43 +0000

    updated triats-api/routes.py

--- traits-api/application/routes.py
+++ traits-api/application/routes.py
@@ -4,7 +4,7 @@ from random import choice
 
 traits = ['Built to Destory', 'Claustrophobia', 'Early Bird', 'Fast Shot', 'Four Eyes', 'Good Natured', 'Heavy Handed', 'Hoarder', 'Hot Blooded', 'Kamikaze', 'Logans Loophole', 'Loose Cannon'. 'Skilled', 'Small Frame', 'Trigger Discipline', 'Wild Wasteland'  ]
 
-@app.route('/get-animal', methods=['GET'])
-def get_animal():
+@app.route('/get-trait', methods=['GET'])
+def get_trait():
     trait = choice(traits)
     return jsonify(trait=trait)
