96f923af2550ba2a10e69c775b5f604d68eb0b43
Christian-Sav <chris.sav@outlook.com>
2022-04-06 08:09:59 +0000

    updated html

--- front-end/application/models.py
+++ front-end/application/models.py
@@ -28,9 +28,9 @@ class Build(db.Model):
     survival = db.Column(db.Integer)
     unarmed = db.Column(db.Integer)
     def __str__(self):
-        build_full = f"In This build your special skills are: \n Strength of :{self.strength} \n Perception of :{self.perception} \n Endurance of :{self.endurance}\n Charisma of :{self.charisma} \n Intelligence of :{self.intelligence} \
-            \n Agility of :{self.agility} \n Luck of :{self.luck} \n Your Tagged Skills are: {self.tag_1}, {self.tag_2} and {self.tag_3} \
-            \n Your Traits are: {self.trait_1} and {self.trait_2} \n And your skill totals are: \n Barter :{self.barter} \n Energy Weapons :{self.energy_weapons} \n Explosives :{self.explosives} \
-            \n Guns :{self.guns} \n Lockpick :{self.lockpick} \n Medicine :{self.medicine} \n Melee Weapons :{self.melee_weapons} \n Repair :{self.repair} \n Science :{self.science} \
-            \n Sneak :{self.sneak} \n Speech :{self.speech} \n Survival :{self.survival} \n Unarmed :{self.unarmed} \n I hope you enjoy this build!"
+        build_full = f"In This build your special skills are: \n Strength of: {self.strength} \n Perception of: {self.perception} \n Endurance of: {self.endurance}\n Charisma of: {self.charisma} \n Intelligence of: {self.intelligence} \
+            \n Agility of: {self.agility} \n Luck of: {self.luck} \n Your Tagged Skills are: {self.tag_1}, {self.tag_2} and {self.tag_3} \
+            \n Your Traits are: {self.trait_1} and {self.trait_2} \n And your skill totals are: \n Barter: {self.barter} \n Energy Weapons: {self.energy_weapons} \n Explosives: {self.explosives} \
+            \n Guns: {self.guns} \n Lockpick: {self.lockpick} \n Medicine: {self.medicine} \n Melee Weapons: {self.melee_weapons} \n Repair: {self.repair} \n Science: {self.science} \
+            \n Sneak: {self.sneak} \n Speech: {self.speech} \n Survival: {self.survival} \n Unarmed: {self.unarmed} \n I hope you enjoy this build!"
         return build_full.replace('\n', ' ')
