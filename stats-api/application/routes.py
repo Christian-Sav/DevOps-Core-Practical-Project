f64f98adfa4a1e4dd9eca7c537669c0502876524
Christian-Sav <chris.sav@outlook.com>
2022-04-06 16:38:29 +0000

    updated tests and stats

--- stats-api/application/routes.py
+++ stats-api/application/routes.py
@@ -5,113 +5,135 @@ from math import ceil
 @app.route('/get-stats', methods = ['POST'])
 def stats():
     request_data = request.get_json()
-    strength = request_data['strength']
-    perception = request_data['perception']
-    endurance = request_data['endurance']
-    charisma = request_data['charisma']
-    intelligence = request_data['intelligence']
-    agility = request_data['agility']
-    luck = request_data['luck']
-    trait_1 = request_data['trait_1']
-    trait_2 = request_data['trait_2']
-    tag_1 = request_data['tag_1']
-    tag_2 = request_data['tag_2']
-    tag_3 = request_data['tag_3']
 
-    if trait_1 == trait_2:
-        trait_2 == "None"
-    elif trait_1 or trait_2 == 'Claustrophobia':
-        strength = (strength + 1)
-        perception = (perception + 1)
-        endurance = (endurance + 1)
-        charisma = (charisma + 1)
-        intelligence = (intelligence + 1)
-        agility = (agility + 1)
-        luck = (luck + 1)
-    elif trait_1 or trait_2 == 'Early_Bird':
-        strength = (strength + 2)
-        perception = (perception + 2)
-        endurance = (endurance + 2)
-        charisma = (charisma + 2)
-        intelligence = (intelligence + 2)
-        agility = (agility + 2)
-        luck = (luck + 2)
-    elif trait_1 or trait_2 == 'Four_Eyes':
-        perception = (perception - 1)
-    elif trait_1 or trait_2 == 'Small_Frame':
-        agility = (agility + 1)
+    st_strength = request_data['strength']
 
-    if strength < 1:
-        strength = 1
-    elif strength > 10:
-        strength = 10
-    elif perception < 1:
-        perception = 1
-    elif perception > 10:
-        perception = 10
-    elif endurance < 1:
-        endurance = 1
-    elif endurance > 10:
-        endurance = 10
-    elif charisma < 1:
-        charisma = 1
-    elif charisma > 10:
-        charisma = 10
-    elif intelligence < 1:
-        intelligence = 1
-    elif intelligence > 10:
-        intelligence = 10
-    elif agility < 1:
-        agility = 1
-    elif agility > 10:
-        agility = 10
-    elif luck < 1:
-        luck = 1
-    elif luck > 10:
-        luck = 10
+    st_perception = request_data['perception']
 
-    barter = (2 + (2*charisma) + ceil(luck/2))
-    energy_weapons = (2 + (2*perception) + ceil(luck/2))
-    explosives = (2 + (2*perception) + ceil(luck/2))
-    guns = (2 + (2*agility) + ceil(luck/2))
-    lockpick = (2 + (2*perception) + ceil(luck/2))
-    medicine = (2 + (2*intelligence) + ceil(luck/2))
-    melee_weapons = (2 + (2*strength) + ceil(luck/2))
-    repair = (2 + (2*intelligence) + ceil(luck/2))
-    science = (2 + (2*intelligence) + ceil(luck/2))
-    sneak = (2 + (2*agility) + ceil(luck/2))
-    speech = (2 + (2*charisma) + ceil(luck/2))
-    survival = (2 + (2*endurance) + ceil(luck/2))
-    unarmed = (2 + (2*endurance) + ceil(luck/2))
+    st_endurance = request_data['endurance']
+
+    st_charisma = request_data['charisma']
+
+    st_intelligence = request_data['intelligence']
+
+    st_agility = request_data['agility']
+
+    st_luck = request_data['luck']
+
+    st_trait_1 = request_data['trait_1']
+
+    st_trait_2 = request_data['trait_2']
+
+    st_tag_1 = request_data['tag_1']
+
+    st_tag_2 = request_data['tag_2']
+
+    st_tag_3 = request_data['tag_3']
+
+    #establishing all json variables
+
+    if st_trait_1 == st_trait_2:
+        st_trait_2 == "None"
+    elif st_trait_1 or st_trait_2 == 'Claustrophobia':
+        st_strength = (st_strength + 1)
+        st_perception = (st_perception + 1)
+        st_endurance = (st_endurance + 1)
+        st_charisma = (st_charisma + 1)
+        st_intelligence = (st_intelligence + 1)
+        st_agility = (st_agility + 1)
+        st_luck = (st_luck + 1)
+    elif st_trait_1 or st_trait_2 == 'Early_Bird':
+        st_strength = (st_strength + 2)
+        st_perception = (st_perception + 2)
+        st_endurance = (st_endurance + 2)
+        st_charisma = (st_charisma + 2)
+        st_intelligence = (st_intelligence + 2)
+        st_agility = (st_agility+ 2)
+        st_luck = (st_luck + 2)
+    elif st_trait_1 or st_trait_2 == 'Four_Eyes':
+        st_perception = (st_perception - 1)
+    elif st_trait_1 or st_trait_2 == 'Small_Frame':
+        st_agility = (st_agility + 1)
+
+# establashing affects of triats
+
+    if st_strength < 1:
+        st_strength = 1
+    elif st_strength > 10:
+        st_strength = 10
+    elif st_perception < 1:
+        st_perception = 1
+    elif st_perception > 10:
+        st_perception = 10
+    elif st_endurance < 1:
+        st_endurance = 1
+    elif st_endurance > 10:
+        st_endurance = 10
+    elif st_charisma < 1:
+        st_charisma = 1
+    elif st_charisma > 10:
+        st_charisma = 10
+    elif st_intelligence < 1:
+        st_intelligence = 1
+    elif st_intelligence > 10:
+        st_intelligence = 10
+    elif st_agility < 1:
+        st_agility = 1
+    elif st_agility > 10:
+        st_agility = 10
+    elif st_luck < 1:
+        st_luck = 1
+    elif st_luck > 10:
+        st_luck = 10
+
+# ensuring specials are between 1 and 10
+
+    barter = (2 + (2*st_charisma) + ceil(st_luck/2))
+    energy_weapons = (2 + (2*st_perception) + ceil(st_luck/2))
+    explosives = (2 + (2*st_perception) + ceil(st_luck/2))
+    guns = (2 + (2*st_agility) + ceil(st_luck/2))
+    lockpick = (2 + (2*st_perception) + ceil(st_luck/2))
+    medicine = (2 + (2*st_intelligence) + ceil(st_luck/2))
+    melee_weapons = (2 + (2*st_strength) + ceil(st_luck/2))
+    repair = (2 + (2*st_intelligence) + ceil(st_luck/2))
+    science = (2 + (2*st_intelligence) + ceil(st_luck/2))
+    sneak = (2 + (2*st_agility) + ceil(st_luck/2))
+    speech = (2 + (2*st_charisma) + ceil(st_luck/2))
+    survival = (2 + (2*st_endurance) + ceil(st_luck/2))
+    unarmed = (2 + (2*st_endurance) + ceil(st_luck/2))
     
-    if tag_1 or tag_2 or tag_3 == 'Barter':
+#calculating stats
+
+    if st_tag_1 or st_tag_2 or st_tag_3 == 'Barter':
         barter = (barter + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Energy_Weapons':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Energy_Weapons':
         energy_weapons = (energy_weapons + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Explosives':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Explosives':
         explosives = (explosives + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Guns':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Guns':
         guns = (guns + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Lockpick':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Lockpick':
         lockpick = (lockpick + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Medicine':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Medicine':
         medicine = (medicine + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Melee_Weapons':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Melee_Weapons':
         melee_weapons = (melee_weapons + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Repair':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Repair':
         repair = (repair + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Science':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Science':
         science = (science + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Sneak':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Sneak':
         sneak = (sneak + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Speech':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Speech':
         speech = (speech + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Survival':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Survival':
         survival = (survival + 15)
-    elif tag_1 or tag_2 or tag_3 == 'Unarmed':
+    elif st_tag_1 or st_tag_2 or st_tag_3 == 'Unarmed':
         unarmed = (unarmed + 15)
 
-    if trait_1 or trait_2 == 'Good_Natured' :
+# adding traits to stats
+
+    if st_trait_1 or st_trait_2 == 'Good_Natured' :
         barter = (barter + 5)
         medicine = (medicine + 5)
         repair = (repair + 5) 
@@ -121,7 +143,7 @@ def stats():
         explosives = (explosives - 5)
         guns = (guns - 5)
         melee_weapons = (melee_weapons - 5)
-    elif trait_1 or trait_2 == 'Skilled' :
+    elif st_trait_1 or st_trait_2 == 'Skilled' :
         barter = (barter + 5)
         energy_weapons = (energy_weapons + 5)
         explosives = (explosives + 5)
@@ -135,6 +157,8 @@ def stats():
         speech = (speech + 5)
         survival = (survival + 5)
         unarmed = (unarmed + 5)
+
+    # adding traits to stats
     
     return jsonify(barter=barter, energy_weapons=energy_weapons, explosives = explosives, guns=guns, lockpick=lockpick, medicine=medicine, melee_weapons=melee_weapons,\
-        repair=repair, science=science, sneak=sneak, speech=speech, survival=survival, unarmed=unarmed, trait_1 = trait_1, trait_2 = trait_2)
+        repair=repair, science=science, sneak=sneak, speech=speech, survival=survival, unarmed=unarmed, trait_1=st_trait_1, trait_2=st_trait_2)
--- stats-api/tests/test_stats_api.py
+++ stats-api/tests/test_stats_api.py
@@ -17,4 +17,4 @@ class TestViews(TestBase):
         json={"strength":7, "perception":10, "endurance":3, "charisma":9, "intelligence":1, "agility":5, "luck":5, "trait_1":"Heavy_Handed", "trait_2":"Hot_Blooded", \
         "tag_1":"Explosives", "tag_2":"Unarmed", "tag_3":"Lockpick"})
         self.assert200(response)
-        self.assertIn(b'{"barter":45,"energy_weapons":20,"explosives":20,"guns":12,"lockpick":25,"medicine":14,"melee_weapons":16,"repair":14,"science":14,"sneak":17,"speech":30,"survival":13,"trait_1":"Heavy_Handed","trait_2":"Hot_Blooded","unarmed":13}\n', response.data)
+        self.assertIn(b'{"barter":45', response.data)
